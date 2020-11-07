4afb3d9ec27e249ca06fc9c5814c717388710dcf
Your Username <you@example.com>
2020-11-06 22:24:10 -0500

    commit tree 2f3fe077fe6d5613a51a6e4bf6dd9ea0e8bb0cb0

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_string = self.split(/\.|\?|\!/)
+    return split_string.count { |string| string != ""}
   end
+
 end
